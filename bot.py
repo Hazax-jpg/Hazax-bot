e749278e8f15f4b7eb143b414d64767755f655cb
Hazax-jpg <hafizzarkasyi93@gmail.com>
2025-09-17 22:04:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+from telegram.ext import Updater, CommandHandler
 
+TOKEN = "8486763392:AAGpT0tanCSku1a1eDcWTnPEdue1RyJbF8c"
+
+def start(update, context):
+    update.message.reply_text("Hello Hazax 🚀! Bot awak dah aktif 24/7.")
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
